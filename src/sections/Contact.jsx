f64d80e9ba0cb7545cbeda17859d972ce8889eb2
Ashwin Kumar Panda <166705353+Ashwin-88@users.noreply.github.com>
2025-09-26 21:12:07 +0530

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -4,7 +4,8 @@ export default function Contact() {
     <div>
       <p><strong>Email:</strong> <a href="mailto:ashwinpanda21@gmail.com" target="_blank" rel="noreferrer">ashwinpanda21@gmail.com/</a></p>
       <p><strong>Phone:</strong> +91-𝟖𝟑𝟐𝟕𝟕𝟕𝟗𝟕𝟐𝟑</p>
-      <p><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/ashwin-kumar-panda/" target="_blank" rel="noreferrer">https://www.linkedin.com/in/ashwin-kumar-panda/</a></p>
-    </div>
+      <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="ashwin-kumar-panda" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/ashwin-kumar-panda?trk=profile-badge">Ashwin Kumar Panda</a></div>
+      </div>
   );
 }
+
